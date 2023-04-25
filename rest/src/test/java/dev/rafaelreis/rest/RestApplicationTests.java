a828fb01ad8b3effba01f3c9ffc98a29c7248250
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2023-04-24 22:59:56 -0400

    First Commit

---
+++ rest/src/test/java/dev/rafaelreis/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafaelreis.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
